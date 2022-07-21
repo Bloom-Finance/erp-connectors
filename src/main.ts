5789e796c82405c8dd913acd24fa803314645973
alexFiorenza <alexfiorenza2012@gmail.com>
2022-07-21 15:38:19 -0300

    Scaffold of erp-connectors

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+function sayMyName(name: string): void {
+  if (name === 'Heisenberg') {
+    console.log("You're right mate 👍");
+  } else {
+    console.log("You're wrong 👎");
+  }
+}
+
+sayMyName('Heisenberg');
